73f304553d74bbc472127ca881f2c18887d9c190
STUDENT Em Rypalska <Em.Rypalska@students.ittralee.ie>
2023-09-28 11:50:17 +0100

    Created repo and added unity project
    Signed-off-by: STUDENT Em Rypalska <Em.Rypalska@students.ittralee.ie>

---
+++ First Project with Goat/Assets/NewBehaviourScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    float currentSpeed, walkingSpeed = 2, runningSpeed = 4;
+    private float turningSpeed = 100;
+    Animator NewBehaviourScript 
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSpeed = walkingSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    
+        if (Input.Getkey(KeyCode.W)) 
+        { 
+            transform.position
+        }
+        if (Input.Getkey(KeyCode.S))
+}
